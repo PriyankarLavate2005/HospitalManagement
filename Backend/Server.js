b7b6aa5b57e82c4045d80125ff3a3ad069ae7764
PriyankarLavate2005 <priyankalavate909@gmail.com>
2025-03-20 11:07:45 +0530

    patientProfile

--- Backend/Server.js
+++ Backend/Server.js
@@ -6,18 +6,14 @@ const connectDB = require('./Models/DB');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
-
-
 connectDB();
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/", authRoutes);
 
-// Start the server
 app.listen(5000, () => {
     console.log("Server is started on port 5000");
 });
